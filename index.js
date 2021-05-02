25683ca885a61d18b78632c558bdac63e0775fec
SergioP <sergio.pinedas94@gmail.com>
2021-05-02 18:28:08 -0500

    Add dynamic list of cars

--- index.js
+++ index.js
@@ -1,8 +1,57 @@
-const Driver = require('./Models/Driver')
-const Car = require('./Models/Car')
-const TrackRace = require('./Models/TrackRace')
+const inquirer = require("inquirer");
+const Driver = require("./Models/Driver");
+const Car = require("./Models/Car");
+const TrackRace = require("./Models/TrackRace");
 
-const driver1 = new Driver('Sergio');
-const driver2 = new Driver('Rolin')
-const carsList = [new Car(driver1, 1), new Car(driver2, 2)]
-const trackRace = new TrackRace(carsList, 5000);
+const driverList = [];
+
+const requestNumberOfPlayes = async () => {
+  const { numberOfPlayers } = await inquirer.prompt({
+    type: "input",
+    name: "numberOfPlayers",
+    validate: (answer) => {
+      if (isNaN(answer)) {
+        return "Por favor ingrese un número";
+      }
+      return true;
+    },
+    message: `***Bienvenido a CarRace***\n\n
+      ingrese el número de jugadores 
+    `,
+  });
+
+  return numberOfPlayers;
+};
+
+const requestName = async () => {
+  const { playerName } = await inquirer.prompt({
+    type: "input",
+    name: "playerName",
+    message: `ingrese el nombre del jugador `,
+  });
+  return playerName;
+};
+
+const createListOfDrivers = async (numberOfPlayers) => {
+  if (!numberOfPlayers) return [];
+  const driversList = [];
+  for (let index = 0; index < numberOfPlayers; index++) {
+    const playerName = await requestName();
+    driversList.push(new Driver(playerName));
+  }
+  return driversList;
+};
+
+const createListOfCars = (driversList) => {
+  if (!driversList || driversList.length <= 0) return [];
+  return driversList.map((driver, index) => new Car(driver, index));
+};
+
+const main = async () => {
+  const numberOfPlayers = await requestNumberOfPlayes();
+  const driversList = await createListOfDrivers(numberOfPlayers);
+  const carsList = createListOfCars(driversList);
+  console.log(carsList);
+};
+
+main();
